a600484231b251ceb57a3efbb7bdabba374f86de
MVeroC <vero.887@hotmail.com>
2022-07-30 20:50:33 -0300

    make it better

--- PI_ConilMV/BackEnd/src/main/java/com/portfolio/mvc/Controller/CEducacion.java
+++ PI_ConilMV/BackEnd/src/main/java/com/portfolio/mvc/Controller/CEducacion.java
@@ -29,7 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "https://portfoliomveroc.web.app")
+@CrossOrigin
 public class CEducacion {
     @Autowired
     Seducacion sEducacion;
